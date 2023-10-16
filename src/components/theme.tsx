6ae43de909e9163086634b9d46c7fcd7996d82c8
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2023-10-15 21:58:13 -0300

    Theme switcher.

---
+++ src/components/theme.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Switch } from "@nextui-org/react";
+import { Moon, Sun } from "@phosphor-icons/react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function Theme() {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  return (
+    <Switch
+      size="lg"
+      color="success"
+      startContent={<Sun />}
+      endContent={<Moon />}
+      isSelected={theme === "light"}
+      onValueChange={(isSelected: boolean) => isSelected ? setTheme('light') : setTheme('dark')}
+    />
+  )
+};
