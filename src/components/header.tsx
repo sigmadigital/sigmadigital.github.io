eec9183a7d2c5d6b1dc60a6991c76a33acfe2add
João Victor T. Salgado <jvictorteo2000@hotmail.com>
2024-03-31 00:08:01 -0300

    Navbar icons.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { GithubLogo, LinkedinLogo, PaperPlaneTilt } from "@phosphor-icons/react";
 
 import { Theme } from "@/components/theme";
 export default function Header() {
@@ -31,11 +32,19 @@ export default function Header() {
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify="end">
+      <NavbarContent className="hidden lg:flex gap-4" justify="end">
         <NavbarItem>
-          <Button color="success" href="#" variant="flat">
-            Get in touch
-          </Button>
+          <div className="flex flex-row mt-2">
+            <Button isIconOnly as={Link} color="success" href="https://github.com/teofilosalgado" variant="flat">
+              <GithubLogo size={28} />
+            </Button>
+            <Button isIconOnly as={Link} className="ml-4" color="success" href="https://www.linkedin.com/in/teofilosalgado/" variant="flat">
+              <LinkedinLogo size={28} />
+            </Button>
+            <Button isIconOnly as={Link} className="ml-4" color="success" href="mailto:jvictorteo2000@hotmail.com" variant="flat">
+              <PaperPlaneTilt size={28} />
+            </Button>
+          </div>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
